c29db5220e647e75c4fc86e994b63247b26eca61
William S <will.step.205@gmail.com>
2023-08-05 18:41:04 -0700

    initial project files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import logo from './logo.svg';
 import './App.scss';
+import { Configuration, OpenAIApi } from 'openai'
 
 function App() {
+
+  const setupTextarea = document.getElementById('setup-textarea')
+  const setupInputContainer = document.getElementById('setup-input-container')
+  const movieBossText = document.getElementById('movie-boss-text')
+
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+  })
+
+  const openai = new OpenAIApi(configuration)
+
+  document.getElementById("send-btn").addEventListener("click", () => {
+    if (setupTextarea.value) {
+      setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
+      movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
+    }
+    console.log("clicked")
+  })
+  
+  async function fetchBotReply() {
+    const response = await openai.createCompletion({
+      'model': 'text-davinci-003',
+      'prompt': 'Sound enthusiastic in five words or less.'
+    })
+    console.log(response)
+  }
+  fetchBotReply()
+  
   return (
     <div className="App">
       <header className="App-header">
