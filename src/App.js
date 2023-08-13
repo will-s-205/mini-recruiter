c6f9d1de99a6bcc7b0ce5a965781a3a54961c7d3
William S <will.step.205@gmail.com>
2023-08-12 20:38:21 -0700

    pdfjs functionality

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import recruiterImg from './assets/images/boss.png'
 import loading from './assets/images/loading.svg'
 import dollar from './assets/images/dollar.png'
 import sendBtn from './assets/images/send-btn-icon.png'
+import toast, { Toaster } from 'react-hot-toast'
+import * as pdfjs from 'pdfjs-dist'
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`
 
 function App() {
-  const speachBubbleText = 'Hi there, I am your new recruiter. I am here to help you find a job. I will ask some questions. Consider it as a phone screen call. What kind of role do you think fits you well? Say JavaScript Engineer, or Python Backend Developer, or something else?'
+  const speachBubbleText = 'Hi, I am your new assistant. I will help you to understand what is in the document. Please ask your questions'
   const speachBubbleTextWait = 'Ok, just wait a second while my digital brain digests that...'
   const [speachBubble, setSpeachBubble] = useState(speachBubbleText)
-  const [advice, setAdvice] = useState('')
   const [isLoading, setIsLoading] = useState(false)
   const [text, setText] = useState('')
 
@@ -20,11 +23,6 @@ function App() {
       handleSubmit();
     }
   }
-
-  // // TRIGGERING ADVICES ONCE SPEACH BUBBLE IS FILLED 
-  useEffect(() => {
-    fetchAdvice(speachBubble)
-  }, [speachBubble])
 
   // API BOT SETUP
   const configuration = new Configuration({
@@ -41,22 +39,46 @@ function App() {
       // https://platform.openai.com/account/rate-limits
       'model': 'text-davinci-003',
       // 'model': 'text-ada-001', // MORE SIMPLE VERSION AND LESS EXPENSIVE
-      prompt: `From now on, you are my interviewer for a ${outline} role. Ask me my first technical question`,
+      prompt: `From now on, you are my agent that shortly answers my questions based on next information: ${outline}`,
       max_tokens: 60,
     }).catch(err => console.log(err))
     setSpeachBubble(response.data.choices[0].text.replace(/^[.!]/, ''))
     setIsLoading(false)
   }
 
-  async function fetchAdvice(outline) {
-    const modifiedData = outline.replace(/you.{1,2}?/gi, 'I')
-    const response = await openai.createCompletion({
-      model: 'text-davinci-003',
-      // 'model': 'text-ada-001', // MORE SIMPLE VERSION AND LESS EXPENSIVE
-      prompt: `${modifiedData}`,
-      max_tokens: 120,
-    }).catch(err => console.log(err))
-    setAdvice(response.data.choices[0].text.trim())
+  const convertPdfToText = async (file) => {
+    try {
+      // GET PDF TOTAL PAGES
+      const pdf = await pdfjs.getDocument({ url: URL.createObjectURL(file) }).promise
+      const totalPages = pdf.numPages
+      const pageTextPromises = []
+
+      // GET TEXT FROM EACH PAGE
+      for (let i = 1; i <= totalPages; i++) {
+        const page = await pdf.getPage(i)
+        const textContent = await page.getTextContent();
+        const textItems = textContent.items.map(item => item.str).join(' ')
+        pageTextPromises.push(textItems)
+      }
+
+      // JOIN ALL PAGES TEXT
+      const allText = await Promise.all(pageTextPromises)
+      setText(allText.join(' '))
+      toast.success("PDF successfully processed")
+    } catch (error) {
+      console.error("Error processing PDF:", error)
+      toast.error("Invalid PDF structure")
+    }
+  }
+
+  // CONVERT PDF TO TEXT
+  const onFileChange = (event) => {
+    const selectedFile = event.target.files[0]
+    if (selectedFile) {
+      convertPdfToText(selectedFile)
+      console.log(selectedFile)
+      fetchBotReply(selectedFile)
+    }
   }
 
   function handleSubmit(e) {
@@ -67,6 +89,7 @@ function App() {
 
   return (
     <div className="App">
+      <Toaster />
 
       {/* HEADER */}
       <header>
@@ -82,6 +105,14 @@ function App() {
               <p id="recruiter-text">{speachBubble}</p>
             </div>
             <img src={recruiterImg} alt="recruiter"></img>
+          </div>
+
+          <div>
+            <input type="file" accept=".pdf" onChange={onFileChange} />
+            {/* <div>
+              <strong>Extracted Text:</strong>
+              <p>{text}</p>
+            </div> */}
           </div>
 
           {/* INPUT AND LOADING */}
@@ -111,7 +142,7 @@ function App() {
         <div className='middleBar'></div>
 
         {/* ADVICE OUTPUT */}
-        {!isLoading ? (
+        {/* {!isLoading ? (
           <section className="output-container" id="output-container">
             <div id="output-img-container" className="output-img-container"></div>
             <h1 id="output-title">Piece of advice</h1>
@@ -129,12 +160,12 @@ function App() {
           <section className="output-container" id="output-container">
             <img src={loading} className="loading2" alt="Loading..."></img>
           </section>
-        )}
+        )} */}
       </main>
 
       {/* FOOTER */}
       <footer>
-        <a href="https://github.com/will-s-205/mini-recruiter" target="_blank" rel="noreferrer">&copy; 2023 by William Step</a>
+        <a href="https://github.com/will-s-205/mini-recruiter" target="_blank" rel="noreferrer">&copy; 2023 by ????????</a>
       </footer>
     </div>
   );
